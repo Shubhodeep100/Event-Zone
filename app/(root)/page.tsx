99ef37138436955de3a852c250417803c7c31c2c
Shubhodeep <banerjeeshubho98@gmail.com>
2024-01-19 22:54:45 +0530

    project cleanup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="">
-      <Button variant="destructive" className="px-6">Hello</Button>
+  
     </main>
   );
 }
