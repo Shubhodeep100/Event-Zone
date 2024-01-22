d403ec780b6662c77fe37a759c375206428ed044
Shubhodeep <banerjeeshubho98@gmail.com>
2024-01-22 22:40:08 +0530

    db new user cretion action

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -3,6 +3,7 @@ import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { createUser } from "@/lib/actions/user.actions";
 import { clerkClient } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
@@ -54,29 +55,30 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
-  if(eventType === 'user.created') {
-    const {id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+  if (eventType === "user.created") {
+    const { id, email_addresses, image_url, first_name, last_name, username } =
+      evt.data;
 
     const user = {
-        clerkId: id,
-        email: email_addresses[0].email_address,
-        username: username!,
-        firstName: first_name,
-        lastName: last_name,
-        photo: image_url,
-
-    }
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
+    };
 
     const newUser = await createUser(user);
 
-if(newUser) {
-    await clerkClient.users.updateUserMetadata(id, {
+    if (newUser) {
+      await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
-            userId: newUser._id
-        }
-    })
-}
+          userId: newUser._id,
+        },
+      });
+    }
 
+    return NextResponse.json({ message: "OK", user: newUser });
   }
 
   return new Response("", { status: 200 });
