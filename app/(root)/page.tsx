38aad5168ef654f51782a71f1725f864138ce209
Shubhodeep <banerjeeshubho98@gmail.com>
2024-01-22 19:30:59 +0530

    home page updates

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">
-              Host, Connect, Celebrate: Your Events, Our Platform!
+              Host, Connect, Celebrate: Your Shows, Our Platform!
             </h1>
             <p className="p-regular-20 md:p-regular-24">Book and Learn helpful tips from 3,456+ mentors in world-class companies with our global community.</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
@@ -17,7 +17,16 @@ export default function Home() {
             </Button>
           </div>
           <Image src="/assets/images/hero.png" alt="Hero Image" width={1000} height={1000} className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]" />
-          
+
+        </div>
+      </section>
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">
+          Trusted by<br />
+          Thousdans of Shows
+        </h2>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          Search CategoryFilter
         </div>
       </section>
     </>
